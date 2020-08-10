1e6d49d6a9485f7a1e65ef4ef5e72ebac2fb8570
Fabiano <fabmarley78@hotmail.com>
2020-08-10 14:53:17 -0300

    ausei o MariaDB para estes exemplos

---
+++ Querys/FornecedorPizzaria.sql
@@ -0,0 +1,10 @@
+select * FROM info_fornecedor;
+
+-- DELETE FROM info_fornecedor WHERE id = 1;
+
+
+ INSERT INTO `info_fornecedor` (`id`, `endereco`, `estado`, `nome`) VALUES (1, 'Rua. Matties Aires, 200', 'RJ', 'Pizzaria Trote de Mestre');
+ INSERT INTO `info_fornecedor` (`id`, `endereco`, `estado`, `nome`) VALUES (2, 'Rua. Bartholomeu Correa Bueno, 116', 'SP', 'Pizzaria Sanches');
+ INSERT INTO `info_fornecedor` (`id`, `endereco`, `estado`, `nome`) VALUES (3, 'Av. Oratório, 1110', 'SP', 'Pizzaria Pappito Mio');
+ INSERT INTO `info_fornecedor` (`id`, `endereco`, `estado`, `nome`) VALUES (4, 'Rua ibiporã, 101', 'SP', 'Pizzaria Mestre Fabiann');
+ INSERT INTO `info_fornecedor` (`id`, `endereco`, `estado`, `nome`) VALUES (5, 'Rua Constantine, 10', 'RJ', 'Pizzaria Constantine');
---
+++ Querys/mariaDbFornecedor.sql
@@ -0,0 +1,5 @@
+select * FROM info_fornecedor;
+
+INSERT INTO info_fornecedor (endereco, estado, nome) VALUES ('Av. Oratório, 1110', 'SP', 'Floricutura do Zé');
+
+DELETE FROM info_fornecedor WHERE id = 4;
